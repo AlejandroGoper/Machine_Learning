eab1b0b762c30c6fbd75a4c445c3a9e5b43b81d5
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-04-27 21:21:32 -0500

    Primer intento creacion de frontera

---
+++ Practica4_Comparativo_de_Clasificadores/FD.py
@@ -0,0 +1,45 @@
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.patches as mpatches
+def hacer_frontera(X,y,clf):
+
+    # Create color maps
+    cmap_light = ListedColormap(['#FFAAAA', '#ffcc99', '#ffffb3','#b3ffff','#c2f0c2'])
+    cmap_bold = ListedColormap(['#FF0000', '#ff9933','#FFFF00','#00ffff','#00FF00'])
+ 
+    # we create an instance of Neighbours Classifier and fit the data.
+    #clf = KNeighborsClassifier(n_neighbors, weights='distance')
+    #clf.fit(X, y)
+    
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, x_max]x[y_min, y_max].
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                            np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+    
+    # Plot also the training points
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold,
+                    edgecolor='k', s=20)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    
+    patch0 = mpatches.Patch(color='#FF0000', label='1')
+    patch1 = mpatches.Patch(color='#ff9933', label='2')
+    #patch2 = mpatches.Patch(color='#FFFF00', label='3')
+    #patch3 = mpatches.Patch(color='#00ffff', label='4')
+    #patch4 = mpatches.Patch(color='#00FF00', label='5')
+    plt.legend(handles=[patch0, patch1])#, patch2, patch3,patch4])
+    
+
+    #plt.title("5-Class classification (k = %i, weights = '%s')"
+     #           % (n_neighbors, weights))
+ 
+    plt.show()
