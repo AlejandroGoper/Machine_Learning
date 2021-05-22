73339bcb658b1859808223369e69a40830e67fa7
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-05-22 16:53:51 -0500

    Sol: errores en la dimensionalidad de los array

--- Practica6_Comparativo_de_clasificadores_y_perceptron_multicapa/PerceptronMulticapa.py
+++ Practica6_Comparativo_de_clasificadores_y_perceptron_multicapa/PerceptronMulticapa.py
@@ -13,6 +13,7 @@ con el resto de scripts de otros clasificadores
 
 from numpy.random import randn
 from numpy import exp,append,ones,dot,array
+from pandas import DataFrame
 
 class PerceptronMulticapa():
     """ Metodo constructor y definicion de atributos de clase"""
@@ -47,26 +48,32 @@ class PerceptronMulticapa():
         return f*(1-f)
     
     """ Propagación hacia adelante a través de la red"""
-    def adelante(self, X):
-        X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
-        self.z1 = dot(X,self.w1) # Producto punto entre X (entradas) y el primer conjunto de 3x2 pesos
+    def adelante(self, dataset):
+        #ds = DataFrame(dataset)
+        #X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
+        dataset['bias'] = 1
+        self.z1 = dot(dataset,self.w1) # Producto punto entre X (entradas) y el primer conjunto de 3x2 pesos
         self.z2 = self.sigmoide(self.z1) # Evaluamos el resultado en la función de activación
         self.z3 = dot(self.z2,self.w2) # Producto punto entre el resultado de las capas ocultas (z2) y el segundo conjunto de 3x1 pesos
         o = self.sigmoide(self.z3) # Funcion de activación final
         return o
     """ Algoritmo backpropagation para los errores """
-    def atras(self,X,y,output,step):
-        X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
-        self.o_error = y - output # error en la salida
+    def atras(self,dataset,y,output,step):
+        ds = DataFrame(dataset)
+        y_ = DataFrame(y)
+        #X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
+        ds['bias'] = 1
+        self.o_error = y_ - output # error en la salida
         self.o_delta = self.o_error*self.sigmoide_prima(output)*step # Aplicando la derivada de la sigmoide al error 
         self.z2_error = self.o_delta.dot(self.w2.T) # z2 error: que tanto contribuye al error de salida nuestra capa oculta de pesos
         self.z2_delta = self.z2_error * self.sigmoide_prima(self.z2)*step # Aplicando la derivada de la sigmoide al error z2
-        self.w1 += X.T.dot(self.z2_delta) # Ajustando los primeros pesos
+        self.w1 += ds.T.dot(self.z2_delta) # Ajustando los primeros pesos
         self.w2 += self.z2.T.dot(self.o_delta) # Ajustando los segundos pesos
     
     """ Método Predict """
-    def predict(self, X):
-        predicted = X.apply(self.adelante,axis=1)
+    def predict(self, dataset):
+        ds = DataFrame(dataset)
+        predicted = ds.apply(self.adelante,axis=1)
         predicted_value = [i[0] for i in predicted]
         umbral = 0.5
         predicted_label = [0 if i < umbral else 1 for i in predicted_value]  
@@ -74,7 +81,9 @@ class PerceptronMulticapa():
     
     """ Método fit"""
     def fit(self, X, y,epochs = 1000, step = 0.01):
+        ds = DataFrame(X)
         for epoch in range(epochs):
-            X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
-            output = self.adelante(X)
-            self.atras(X, y, output, step)
+            ds['bias'] = 1
+            #X = append(X,ones((len(X),1)),axis=1) # Agregamos 1 a las entradas para añadir el bias
+            output = self.adelante(ds)
+            self.atras(ds, y, output, step)
