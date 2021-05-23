985833e5e962606c193bc32b37e40ea4a6ff7bb8
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-05-23 12:54:07 -0500

    Final, desordenando dataset4 y aumentando puntos

--- Practica6_Comparativo_de_clasificadores_y_perceptron_multicapa/main.py
+++ Practica6_Comparativo_de_clasificadores_y_perceptron_multicapa/main.py
@@ -23,7 +23,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 #from sklearn.linear_model import Perceptron as Per
 from pandas import DataFrame,concat
-from numpy.random import seed,normal
+from numpy.random import seed,normal,shuffle
 
 
 # Importamos los archivos .csv y arreglamos en la forma tradicional
@@ -55,11 +55,12 @@ mu_x, sigma_x = 0, 0.1
 
 # Creando la distribucion
 
-d1 = DataFrame({'x1':normal(mu_x,sigma_x,1000)+1,'x2':normal(mu_x,sigma_x,1000)+1,'type':0})
-d2 = DataFrame({'x1':normal(mu_x,sigma_x,1000)+1,'x2':normal(mu_x,sigma_x,1000)-1,'type':1})
-d3 = DataFrame({'x1':normal(mu_x,sigma_x,1000)-1,'x2':normal(mu_x,sigma_x,1000)-1,'type':0})
-d4 = DataFrame({'x1':normal(mu_x,sigma_x,1000)-1,'x2':normal(mu_x,sigma_x,1000)+1,'type':1})
+d1 = DataFrame({'x1':normal(mu_x,sigma_x,200)+1,'x2':normal(mu_x,sigma_x,200)+1,'type':0})
+d2 = DataFrame({'x1':normal(mu_x,sigma_x,200)+1,'x2':normal(mu_x,sigma_x,200)-1,'type':1})
+d3 = DataFrame({'x1':normal(mu_x,sigma_x,200)-1,'x2':normal(mu_x,sigma_x,200)-1,'type':0})
+d4 = DataFrame({'x1':normal(mu_x,sigma_x,200)-1,'x2':normal(mu_x,sigma_x,200)+1,'type':1})
 dataset4 = array(concat([d1,d2,d3,d4],ignore_index=True))
+shuffle(dataset4) # desordenando datos del dataset
 
 
 x_4 = dataset4[:,:-1] # Eliminando la ultima columna
@@ -69,6 +70,8 @@ y_true_4 = dataset4[:,-1] # Tomamos solo la ultima columna
 datasets = [x_1,x_2,x_3,x_4]
 labels = [y_true_1,y_true_2,y_true_3,y_true_4]
 
+#datasets = [x_4]
+#labels = [y_true_4]
 
 clasificadores = [MinimaDistancia(),KNeighborsClassifier(n_neighbors=5),
                   SVC(kernel="rbf",C = 10, gamma=0.1),Perceptron(w0=1,w1=0.1, w2=0.1),
