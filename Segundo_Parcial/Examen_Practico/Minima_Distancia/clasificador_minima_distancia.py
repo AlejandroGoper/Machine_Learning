99357c33cfa8a57adcd9700f15da9a2993637626
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-06-01 13:31:47 -0500

    MDC funcional para mas de dos clases

--- Segundo_Parcial/Examen_Practico/Minima_Distancia/clasificador_minima_distancia.py
+++ Segundo_Parcial/Examen_Practico/Minima_Distancia/clasificador_minima_distancia.py
@@ -1,62 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-    Autor: I. Alejandro Gómez
+Created on Tue Jun  1 12:24:23 2021
 
-    Este código es para realizar la clasificacion por minima distacia.
-    Basicamente recibe un dataset de entrenamiento, otro de prueba y las etiquetas, hace el promedio
-    de todos los puntos de cada clase en el dataset entrenamiento para encontrar el centroide de cada
-    grupo y luego para cada punto del dataset de prueba verifica de que centro está más cerca y a ese
-    lo asigna como predicción
+@author: alejandro_goper
+
+Clasificador por minima distancia valido para datasets con m�s de dos clases
+
+Localiza los centroides de cada clase en la parte del entrenamiento, luego, 
+para cada nuevo punto, calcula la distancia a cada centroide y asigna al nuevo
+punto la etiqueta de la clase m�s cercana.
+
 """
+
+""" Cargando modulos necesarios """
 
 from numpy import array,unique,where
 from pandas import DataFrame
 
-class MinimaDistancia():
-    # Método constructor
+class MDC():
+    # Metodo constructor
     def __init__(self):
+        # Atributo de clase
         self.centros_de_clase = None
 
     """
-        Método Fit: Realiza el entrenamiento del clasificador
+        Metodo Fit: Realiza el entrenamiento del clasificador
 
-        Parámetros: 
-            - training_ds: Datos de prueba
-            - training_y_labels: Etiquetas de los datos de prueba
+        Parametros: 
+            - training_ds: Datos de entrenamiento
+            - training_y_labels: Etiquetas de los datos de entrenamiento
     """
     def fit(self,trainning_ds,trainning_y_labels):
         # Utilizando pandas para el filtrado de las clases
+        # Creamos un dataset con los valores de training_ds
         df = DataFrame(trainning_ds,columns=["X","Y"])
+        # Agregamos una columna con las etiquetas correspondientes
         df["Label"] = trainning_y_labels
-        # Obtengo en un arreglo las unicas clases diferentes
+        
+        # Obtengo en un arreglo las unicas clases diferentes ordenadas
         clases = unique(trainning_y_labels)
         # Para cada clase realizo el promedio de todos sus valores en el dataset
         centroides = []
         for clase in clases:
-            #Filtro el dataset por su valor de etiquetas
+            #Filtro el dataset por su valor de etiquetas unicas de clase
             df_filtrado = df[df["Label"] == clase]
             Sum_X = df_filtrado["X"].sum() #Sumo toda la columna de las componentes X
             Sum_Y = df_filtrado["Y"].sum() #Sumo toda la columna de las componentes Y
-            n = len(df_filtrado["X"])
+            n = len(df_filtrado["X"]) # Como la dimension de X es igual a la de Y 
             centroides.append([Sum_X/n,Sum_Y/n]) #Guardo el punto promedio de cada clase en una lista
+        
+        #Agrego las coordenadas de los centroides como atributo de clase
         self.centros_de_clase = array(centroides) # Convierto la lista a un arreglo numpy
     
     """
-        Método Predict: Realiza las predicciones con el clasificador entrenado
+        Metodo Predict: Realiza las predicciones con el clasificador entrenado
 
-        Parámetros:
+        Parametros:
             - test_ds: Data set de prueba
         Regresa:
             - predicted_labels: Array tipo numpy con las etiquetas predichas
     """
     def predict(self,test_ds):
-        # Realizo la clasificación por minima distancia
+        # Realizo la clasificacion por minima distancia
         # Calculo la distancia de cada punto en test_ds a cada uno de los centroides
         predicted_labels = []
         centroides = self.centros_de_clase
         for punto in test_ds:
             diff = punto-centroides # Esto me genera una matriz con las distancia del punto pivote a cada centroide
             d2 = diff[:,0]*diff[:,0] + diff[:,1]*diff[:,1] # Calculo la distancia al cuadrado
+            # En el arreglo d2 encuentro el indice de su valor mas peque�o
             indice_valor_minimo = where(d2 == d2.min())
+            # la linea anterior regresa una matriz de tipo float de dim [1x1]
+            # lo convertimos simplemente a int
             indice_valor_minimo = int(indice_valor_minimo[0])
+            # el indice del valor minimo corresponde con la clase predicha
             predicted_labels.append(indice_valor_minimo)
         return array(predicted_labels)
